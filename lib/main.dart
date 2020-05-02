6197901c373745a6e8086715cb22815d6c4138e5
santosh <sklearn21@gmail.com>
2020-05-02 16:05:57 +0530

    Added registration_screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
 
 void main() => runApp(FlashChat());
 
@@ -13,7 +14,7 @@ class FlashChat extends StatelessWidget {
               body1: TextStyle(
         color: Colors.black54,
       ))),
-      home: LoginScreen(),
+      home: RegistrationScreen(),
     );
   }
 }
